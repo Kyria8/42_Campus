14ef1e6335baef818433975ef2d5b8ed5dd78fd9
Veronica Montero cabello <vmontero@c1r10s2.42madrid.com>
2021-05-13 13:53:43 +0200

    Otro intento

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vmontero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 14:11:42 by vmontero          #+#    #+#             */
-/*   Updated: 2021/05/11 18:44:52 by vmontero         ###   ########.fr       */
+/*   Updated: 2021/05/13 13:52:26 by vmontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@
 char	*ft_select(char *src, char end)
 {
 	int		i;
-	int		j;
+	//int		j;
 	char	*res;
-	char	*aux;
+	//char	*aux;
 
 	i = 0;
-	j = 0;
 	printf("-Acabo de entrar y src es: .......%s\n", src);
 	while (src[i] != end)
 	{
@@ -33,13 +32,8 @@ char	*ft_select(char *src, char end)
 	}
 	printf("-Soy la cantidad de caracteres que tengo %d\n", i);
 	res = malloc(sizeof(char) * i + 1);
-	while (i-- > 0)
-	{
-		res[j] = src[j];
-		j++;
-		//printf("-Estoy dentro del bucle de la funcion y res va siendo: ......%c\n", res[j]);
-	}
-	res[j] = '\0';
+	res = ft_substr(src, 0, i);
+	res[i] = '\0';
 	//aux = res;
 	//free(res);
 	printf("-Estoy en la funcion select\n");
@@ -47,7 +41,7 @@ char	*ft_select(char *src, char end)
 	return (res);
 }
 
-char	*cutline(char **s)
+char	*cutline(char **s, char *line)
 {
 	int		i;
 	char	*temp;
@@ -55,20 +49,14 @@ char	*cutline(char **s)
 	i = 0;
 	while ((*s)[i] != '\n' && (*s)[i] != '\0')
 	   i++;
-	if ((*s)[i] == '\n')
+	if (i < ft_strlen(*s))
 	{
-		temp = ft_substr(*s, 0, i);
-		*s = ft_strdup(&(*s)[i + 1]);
-	}
-	else if ((*s)[i] == '\0')
-	{
-		while((*s)[--i] != '\n')
-			i--;
-		temp = ft_strdup(*s);
+		*line = ft_substr(*s, 0, i);
+		temp = ft_substr(*s, i, ft_strlen(*s));
 		free(*s);
-		*s = NULL;
+		*s = temp;
 	}
-	return (temp);
+	return (line);
 }
 
 int	get_next_line(int fd, char **line)
@@ -82,12 +70,30 @@ int	get_next_line(int fd, char **line)
 		printf("PATADA EN LA ESPINILLA");
 		return (-1);
 	}
-	temp = "";
 	while (read(fd, buff, BUFFER_SIZE) > 0)
 	{
-		temp = ft_strjoin(temp, buff);
-
+		if (!s[fd])
+			s[fd] = ft_strdup(buff);
+		else
+		{
+			temp = ft_strjoin(s[fd], buff);
+			free(s[fd]);
+			s[fd] = temp;
+		}
+		if (ft_strchr(s[fd], '\n'))
+			break;
 	}
+	if (!s[fd] && (read(fd, buff, BUFFER_SIZE) <= 0))
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	else
+	{
+		*line = cutline(&s[fd], *line);
+		//s[fd] = cutline(&s[fd]);
+	}
+	return (1);
 }
 
 int main(void)
